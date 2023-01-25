84295957f9ac54b1d02fb8e3aec5c56530e7c59a
Jorge Amestoy <jamestoysabucedo@danielcastelao.org>
2023-01-25 09:19:49 +0100

    Commit Primer Squash COlaborador

---
+++ src/ColaboradorAngi.java
@@ -0,0 +1,7 @@
+public class ColaboradorAngi {
+    //Realizo una modificación en el código y hago 1er. commit
+
+    //Realizo segunda modificación en el código y hago 2do. commit
+
+    //Realizo tercera modificación en el código y hago 3er. commit
+}
